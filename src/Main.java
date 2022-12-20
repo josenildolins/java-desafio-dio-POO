a0ce16fc0da99ef99ca061590e710464d620e417
josenildolins <josenildo.lins@gmail.com>
2022-12-20 16:26:55 -0300

    Adicionar as classes Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso java = new Curso();
+        java.setTitulo("Java");
+        java.setDescricao("descrição aqui");
+        java.setCargaHoraria(8);
+
+        Curso javaScript = new Curso();
+        javaScript.setTitulo("Java");
+        javaScript.setDescricao("descrição aqui");
+        javaScript.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição aqui");
+        mentoria.setData(LocalDate.now());
+
+
+        
+        
+        
+        System.out.println(java);
+        System.out.println(javaScript);
+        System.out.println(mentoria);
+
+
+    }
+}
